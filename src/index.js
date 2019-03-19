ee8cb62347c40003cb9f0d3de63408648c388b33
ameerthehacker <ameerjhanprof@gmail.com>
2019-03-19 10:43:35 +0530

    chore: add react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
